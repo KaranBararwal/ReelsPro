9b15d1c677b51f5c00cf89baa6f2d02db2640a5c
Karan <northsentinelhacker@gmail.com>
2025-02-14 16:07:56 +0530

    Add ImageKit authentication API and provider integration

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
 import { withAuth } from "next-auth/middleware"
-
 import { NextResponse } from "next/server";
 
 export default withAuth(
@@ -30,7 +29,6 @@ export default withAuth(
         }
     }
 )
-
 
 // now making the config files for deciding when the middleware function is going to run
 
