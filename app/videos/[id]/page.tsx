fd9637b6992b4e8bf3dbab5d91234c790866fb06
Karan <northsentinelhacker@gmail.com>
2025-02-21 15:41:43 +0530

    Refactor video page component to remove unused import of useSearchParams

--- app/videos/[id]/page.tsx
+++ app/videos/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 // import { useRouter, useSearchParams } from "next/navigation";
-import {  useParams, useSearchParams } from "next/navigation";
+import {  useParams } from "next/navigation";
 import { IVideo } from "@/models/Video";
 import { apiClient } from "@/lib/api-client";
 
